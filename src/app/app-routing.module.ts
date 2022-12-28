80d7ba69463c7ddd8ad1aa70400da2499bbf5096
Your Name <decierdo.dexter@dnsc.edu.ph>
2022-12-28 12:58:07 +0800

    1st Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,21 +2,17 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
-  {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
-  },
+ 
+  { path: '', redirectTo: 'movies', pathMatch: 'full' },
+  { path: 'movies', loadChildren: () => import('./pages/movies/movies.module').then( m => m.MoviesPageModule) },
+  { path: 'movie/:id', loadChildren: () => import('./pages/movie-details/movie-details.module').then( m => m.MovieDetailsPageModule) },
+
 ];
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
   ],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey: '362dd52fa4cb3bcfe192011eb812ca1a',
+  baseUrl: 'https://api.themoviedb.org/3',
+  images: 'http://image.tmdb.org/t/p',
 };
 
 /*
